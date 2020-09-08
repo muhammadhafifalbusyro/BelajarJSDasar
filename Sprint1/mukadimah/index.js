23e2d52cb698873ad06086e17f6c201901706e6d
Muhammad Hafif Al Busyro <muhammadhafifalbusyr0@gmail.com>
2020-09-08 09:21:03 +0700

    add project

---
+++ Sprint1/mukadimah/index.js
@@ -0,0 +1,80 @@
+// document.write("hello world");
+// console.log("ini adalah console");
+
+//variabel
+// variabel adalah wada/ tempat untuk menyimpan tipe data
+
+//integer
+//1,2,3,0.5
+
+//string
+//tipedata yang diawali dan diakhiri dengan tanda kutip 2
+//bentuknya itu adalah kata atau kalimat atu huruf
+
+//pendeklarasian variabel
+
+// var namaSaya = "muhammad hafif";
+// var angka = 12;
+// var bool = true;
+
+// document.write(namaSaya);
+// document.write("<br>");
+// document.write(angka);
+
+// document.write(bool);
+
+//operator
+
+//aritmatika
+// var nilai1 = 10;
+// var nilai2 = 200;
+// var aritmatkea = nilai1 % 3;
+
+// document.write(nilai1 + nilai2);
+// document.write("<br>");
+// document.write(nilai2 - nilai1);
+// document.write("<br>");
+// document.write(aritmatkea);'
+
+// var nilai2 = 3;
+// nilai2 *= 100;
+// nilai2 /= 2;
+// document.write(nilai2);
+
+// operator perbandingan
+// untuk membandingkan dua buah nilai
+
+// var nilai1 = 2;
+// var nilai2 = 12;
+
+// var perbanding = nilai1 <= nilai2;
+
+// document.write(perbanding);
+
+// 1. mencari luas persegi panjang
+// diketahui panjang = 10 , lebar = 12
+// berapa luas persegi panjang tersebut ?
+
+// var namaDepan = "Hafif";
+// var namaBelakang = "Al Busyro";
+
+// var namaLengkap = namaDepan + namaBelakang;
+// document.write(namaLengkap);
+
+// var panjang = 10;
+// var lebar = 12;
+// var luas = panjang * lebar;
+
+// document.write("luas dari persegi panjang adalah " + luas);
+
+//sebuah lingkarang berdiameter  = 9
+//// buatlah program hitung luas dan keliling lingkaran dengan output pada halaman web
+
+// keliling lingkaran = ...
+// luas lingkaran = ...
+
+// var a = 12;
+// var a = 10;
+
+// // document.write(a);
+// console.log(a);
---
+++ Sprint1/mukadimah/tugas1.js
@@ -0,0 +1,32 @@
+// 1 . diketahui panjang = 10 cm, lebar = 5 cm , dan tinggi = 8 cm. Hitunglah luas permukaan balok dan volume balok sehingga menampilkan otuput pada web
+
+//diketahui :
+//p = ... cm
+//l= ... cm
+//t=... cm
+
+//ditanya ?
+// volume dari balok adalah ... cm3
+//luas permukaan balok adalah .. cm2
+//volume balok dalam liter .. l
+// volume balok ketika di perbesar 3 kali ... cm3
+
+// 2. diketahu segitiga memiliki alas = 10 cm, tinggi = 12 cm. Hitunglah luas segitiga , dan memiliki keluaran output pada web sbb
+
+// diketahui:
+//alas=..cm,
+//tinggi=..cm
+
+//ditanya?
+// luas segitiga adalah ... m2
+
+var panjang = 120;
+var lebar = 100;
+var luas = panjang * lebar;
+var br = "<br>";
+
+document.write("Diketahui :" + br);
+document.write("p = " + panjang + " cm" + br);
+document.write("l = " + lebar + " cm" + br);
+document.write("Ditanya ?" + br);
+document.write("luasnya adalah " + luas + "cm");
